a172f1ba1cc2741d090e4d2ae812203a1b21d6c7
Mr.Scratchcat <118802270+MrScratchcat@users.noreply.github.com>
2023-03-08 10:28:10 +0100

    Update minecraft_server_creator.sh

--- minecraft_server_creator.sh
+++ minecraft_server_creator.sh
@@ -93,12 +93,159 @@ do
         ;;
     esac
 done
+cmd=(dialog --menu "Please Select your difficulty:" 22 76 16)
+options=(
+1 "Easy"
+2 "Normal"
+3 "Hard"
+)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+    case $choice in
+    1)
+        #easy
+        continue=1
+        difficulty=easy
+        ;;
+    2)
+        #normal
+        continue=1
+        difficulty=normal
+        ;;
+    3)
+        #hard
+        continue=1
+        difficulty=hard
+        ;;
+
+    esac
+done
+cmd=(dialog --menu "Please Select your render distance:" 22 76 16)
+options=(
+1 "10"
+2 "16"
+3 "32"
+)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+    case $choice in
+    1)
+        #10
+        continue=1
+        distance=10
+        ;;
+    2)
+        #16
+        continue=1
+        distance=16
+        ;;
+    3)
+        #32
+        continue=1
+        distance=32
+        ;;
+
+    esac
+done
+cmd=(dialog --menu "Please Select your gamemode:" 22 76 16)
+options=(
+1 "Survival"
+2 "Creative"
+3 "Hardcore"
+)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+    case $choice in
+    1)
+        #survival
+        continue=1
+        hardcore=false
+        gamemode=survival
+        ;;
+    2)
+        #creative
+        continue=1
+        hardcore=false
+        gamemode=creative
+        ;;
+    3)
+        #hardcore
+        continue=1
+        hardcore=true
+        gamemode=hardcore
+        difficulty=hard
+        ;;
+
+    esac
+done
 if [ $continue -eq 0 ]; then
     echo "you didnt made a choice, stop script" 
     exit 1
 else
-    sudo chown -R $USER: $HOME
-    sudo chmod +x minecraft_server_downloader.sh
+    echo "#Minecraft server properties
+allow-flight=true
+allow-nether=true
+broadcast-console-to-ops=true
+broadcast-rcon-to-ops=true
+difficulty=${difficulty}
+enable-command-block=false
+enable-jmx-monitoring=false
+enable-query=false
+enable-rcon=false
+enable-status=true
+enforce-secure-profile=true
+enforce-whitelist=false
+entity-broadcast-range-percentage=100
+force-gamemode=false
+function-permission-level=2
+gamemode=${gamemode}
+generate-structures=true
+generator-settings={}
+hardcore=${hardcore}
+hide-online-players=false
+initial-disabled-packs=
+initial-enabled-packs=vanilla
+level-name=world
+level-seed=
+level-type=minecraft\:normal
+max-chained-neighbor-updates=1000000
+max-players=20
+max-tick-time=60000
+max-world-size=29999984
+motd=A Minecraft Server
+network-compression-threshold=256
+online-mode=true
+op-permission-level=4
+player-idle-timeout=0
+prevent-proxy-connections=false
+pvp=true
+query.port=25565
+rate-limit=0
+rcon.password=
+rcon.port=25575
+require-resource-pack=false
+resource-pack=
+resource-pack-prompt=
+resource-pack-sha1=
+server-ip=
+server-port=25565
+simulation-distance=${distance}
+spawn-animals=true
+spawn-monsters=true
+spawn-npcs=true
+spawn-protection=16
+sync-chunk-writes=true
+text-filtering-config=
+use-native-transport=true
+view-distance=${distance}
+white-list=false" > server.properties
+
     mem=$(free -h | grep -i mem | awk '{print int($2 + 0.5)}')
     echo " "
     echo "Allocating ${mem}GB of RAM for Minecraft server."
@@ -108,4 +255,5 @@ else
     echo eula=true > eula.txt
     sudo chmod +x start.bat
     sudo ./start.bat
+    sudo chown -R $USER: $HOME
 fi
